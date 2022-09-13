a58f17911e3a60c02c3efde95d1613f0cf0eaad7
marialindblom <>
2022-09-13 10:15:13 +0300

    lataus lisätty

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useSigninCheck } from "reactfire";
 import Channel from "./components/Channel";
 import SignInOutButton from "./components/SignInOutButton";
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function App() {
   const { status, data: signInCheckResult } = useSigninCheck();
 
   if (status === "loading") {
-    return <span>loading...</span>;
+    return <CircularProgress />;
   }
 
   return (
