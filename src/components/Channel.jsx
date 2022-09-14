04b367763ffecf5941b9ef58ef54159c0d623386
marialindblom <>
2022-09-14 10:49:18 +0300

    header lisätty, mut ei näy

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -3,6 +3,79 @@ import { useFirestore, useFirestoreCollectionData } from "reactfire";
 import Form from "./Form";
 import Messages from "./Messages";
 import CircularProgress from '@mui/material/CircularProgress';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import { Menu, MenuItem } from "@mui/material";
+
+export function MenuAppBar () {
+  const [AuthenticatorAssertionResponse, setAuth] = React.useState(true);
+  const [anchorE1, setAnchorE1] = React.useState(null);
+
+  const handleChange = (event) => {
+    setAnchorE1(event.currentTarget);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorE1(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorE1(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow:1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            arialabel="menu"
+            sx={{ mr:2 }}
+            >            
+            <MenuIcon />
+          </IconButton>
+          {auth && (
+            <div>
+              <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu 
+                id="menu-appbar"
+                anchorEl={anchorE1}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorE1)}
+                onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Profiili</MenuItem>
+                  <MenuItem onClick={handleClose}>Käyttäjä</MenuItem>
+                </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
 
 export default function Channel() {
   const firestore = useFirestore();
