798a814cf2c0deb7d593257e9c9bc3795fe2a10f
marialindblom <>
2022-09-20 14:03:47 +0300

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,12 +1,11 @@
 import Message from "./Message";
-import { Stack } from "@mui/material";
 
 export default function Messages({ messages }) {
   return (
-    <Stack>
+    <div>
       {messages?.map((message) => (
         <Message key={message.id} {...message} />
       ))}
-    </Stack>
+    </div>
   );
 }
