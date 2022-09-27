a2476b2a96d2f90ae25f97ad241ef172af626f98
marialindblom <>
2022-09-27 10:55:29 +0300

    oman viestin poisto

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,8 @@
 import { Typography } from '@mui/material';
 import Chip from '@mui/material/Chip';
+import { Button } from '@mui/material';
+import { deleteDoc, doc } from "firebase/firestore";
+import { useFirestore, useUser } from "reactfire";
 
 const dateTimeFormat = new Intl.DateTimeFormat("en-GB", {
   hour: "numeric",
@@ -8,9 +11,13 @@ const dateTimeFormat = new Intl.DateTimeFormat("en-GB", {
   hour12: false,
 });
 
-export default function Message({ createdAt, text, displayName }) {
+export default function Message({ createdAt, text, displayName, id, uid }) {
+  const { data: user } = useUser();
+  const firestore = useFirestore();
+
   return (
-      <Chip sx={{marginBottom: 1,height:"100%"}} label={<Typography sx={{whiteSpace:"normal"}}>
+    <>
+    <Chip sx={{marginBottom: 1,height:"100%"}} label={<Typography sx={{whiteSpace:"normal"}}>
       {createdAt?.seconds ? (
         <span>{dateTimeFormat.format(new Date(createdAt.seconds * 1000))}</span>
       ) : null}
@@ -18,6 +25,16 @@ export default function Message({ createdAt, text, displayName }) {
       <strong>        
         {displayName ? displayName : null}
       </strong>{" "}
-      {text}</Typography>} variant="outlined"  />
-  );
+      {text} </Typography>} variant="outlined"  />
+      {uid === user.uid ? (
+        <Button variant="contained"
+          onClick={async () => {
+            await deleteDoc(doc(firestore, "messages", id));
+          }}
+          >
+            Poista
+            </Button>
+      ) : null}
+       </>
+  );    
 }
